e9858f61adbbeadc002ad035fb5aac8ff642f5f8
markomate <GCAS022005@coderacademy.edu.au>
2021-03-24 22:02:07 +1100

    Devise recoverable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,21 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Show errors
+  config.action_mailer.raise_delivery_errors = true
+
+  # Devise email config variables
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "mail.google.com",
+    :user_name => ENV["mailer_email"],
+    :password => ENV["mailer_password"],
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :openssl_verify_mode => 'none'
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,24 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Configure actionmailer default url for devise
+  config.action_mailer.default_url_options = { host: 'https://petplace-marko.herokuapp.com'}
+
+  # Show errors
+  config.action_mailer.raise_delivery_errors = true
+
+  # Devise email config variables
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "mail.google.com",
+    :user_name => ENV["mailer_email"],
+    :password => ENV["mailer_password"],
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :openssl_verify_mode => 'none'
+  }
 end
